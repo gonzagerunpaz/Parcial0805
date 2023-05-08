9585a19618fd1d820754c7696887fb25d5d70d06
gonzagerunpaz <gerardo.gonzalez@docentes.unpaz.edu.ar>
2023-05-08 19:26:43 -0300

    comenzando con express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo!!!!, como estas?')
+const express = require('express')
+const PORT = 3002
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.status(200).json({"mensaje": "Hola mundo!!"})
+})
+
+
+app.listen(PORT, ()=>{console.log(`A romperla en el puerto ${PORT}`)})
+
