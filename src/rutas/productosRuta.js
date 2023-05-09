cc161444b7f444bb93e05b9b3f7d4a775a3f6bd0
gonzagerunpaz <gerardo.gonzalez@docentes.unpaz.edu.ar>
2023-05-08 21:21:45 -0300

    Get y post

--- src/rutas/productosRuta.js
+++ src/rutas/productosRuta.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getProductos, getProductoById } = require('../controladores/productosController')
+const { getProductos, getProductoById, postProducto } = require('../controladores/productosController')
 
 const productosRuta = express.Router()
 
 productosRuta.get("/", getProductos)
 productosRuta.get("/:id", getProductoById )
+productosRuta.post("/", postProducto)
 
 
 module.exports = {  productosRuta  }
